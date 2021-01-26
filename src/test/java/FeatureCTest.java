93dbc6a3752d02732a5d1a30cee90cba86cd2fd6
adnan <adnaninci86@gmail.com>
2021-01-26 13:40:22 -0600

    more work is done in feature_c

--- src/test/java/FeatureCTest.java
+++ src/test/java/FeatureCTest.java
@@ -3,5 +3,7 @@ public class FeatureCTest {
     public static void main(String[] args) {
 
         System.out.println("first work is done in feature c");
+
+        System.out.println("more work is done");
     }
 }
