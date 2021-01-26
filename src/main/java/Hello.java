d86481d61b706c7941c9c4959e2b6343d9876ab4
adnan <adnaninci86@gmail.com>
2021-01-26 14:59:41 -0600

    hello commit for master branch

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("do not change this line!");
+        System.out.println("hello world");
 
         System.out.println("b21 is the best!!!");
 
