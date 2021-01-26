2e7dbd271ab92b11cacbe6bd1208128b31d3ea25
adnan <adnaninci86@gmail.com>
2021-01-26 14:53:31 -0600

    new function added into the master branch

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -9,6 +9,7 @@ public class Hello {
         System.out.println("lets learn java!");
 
         System.out.println("lets learn selenium web driver!");
+        System.out.println("new function added");
     }
 
 }
